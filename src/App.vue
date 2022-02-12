2c6306b03419f3565c18a6c7c98451050a1a0367
fabian <fabiannull@outlook.com>
2022-02-12 17:17:22 +0100

    countdowntimer läuft

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <CountDownTimer></CountDownTimer>
+  </div>
 </template>
 
 <script>
+import CountDownTimer from './components/CountDownTimer.vue';
+
 export default {
   name: 'App',
-  components: {},
+  components: { CountDownTimer },
+  data: function () {
+    return {};
+  },
 };
 </script>
 
-<style></style>
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+:root {
+  box-sizing: border-box;
+  text-size-adjust: none;
+  -webkit-text-size-adjust: none;
+  position: relative;
+  font-family: system-ui, 'Apple Color Emoji', 'Segoe UI Emoji',
+    'Segoe UI Symbol', 'Noto Color Emoji';
+  min-height: 100%;
+}
+body {
+  position: relative;
+  height: 100%;
+  min-height: 100vh;
+  font-size: 100%;
+  line-height: 1.5;
+}
+
+input,
+textarea,
+button {
+  font-size: inherit;
+  font-family: inherit;
+}
+</style>
---
+++ src/components/CountDownTimer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="ContDownTimer">
+    {{ daysToCountTo }}
+    {{ hoursToCountTo }}
+    {{ minutesToCountTo }}
+    {{ secondsToCountTo }}
+
+    {{ dateToCountTo }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountDownTimer',
+  components: {},
+  data: function () {
+    return {
+      dateToCountTo: null,
+      daysToCountTo: null,
+      hoursToCountTo: null,
+      minutesToCountTo: null,
+      secondsToCountTo: null,
+    };
+  },
+  methods: {
+    getTimeToDeadlineDate() {
+      var countDownDate = new Date('March 1, 2022 12:00:00').getTime();
+      countDownDate = Math.floor(countDownDate + 691200000); // for Livedemo always current date + 8 days
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      this.daysToCountTo = days;
+      this.hoursToCountTo = hours;
+      this.minutesToCountTo = minutes;
+      this.secondsToCountTo = seconds;
+    },
+
+    setDeadlineDate() {
+      setInterval(this.getTimeToDeadlineDate, 1000);
+    },
+  },
+  created() {
+    this.setDeadlineDate();
+  },
+};
+</script>
+
+<style scoped></style>
